47c6feb43b564cf9313702a1a59b3c3f1ca934a8
jeevan <jeevan2961@gmail.com>
2020-08-24 23:00:42 +0530

    Product Hunt Project

---
+++ producthunt/urls.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from django.urls import path
+import products.views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',products.views.home, name='home'),
+]
